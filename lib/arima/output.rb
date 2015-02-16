bbd86647fb3034482ba59211312d5e174d928396
hisaichi5518 <hisaichi5518@gmail.com>
2015-02-16 17:16:06 +0900

    use Arima::Schema at Arima::Output and Arima::Searcher

--- lib/arima/output.rb
+++ lib/arima/output.rb
@@ -1,25 +1,13 @@
 module Arima
   module Output
+    extend Arima::Schema
 
-    DEFAULT_SCHEMA = [
+    schema_name :output_schema
+    default_schema [
       {"name"=>"time", "jp"=>"行動時間"},
       {"name"=>"uid", "jp"=>"ユーザーid"},
       {"name"=>"type", "jp"=>"行動タイプ"},
       {"name"=>"other", "jp"=>"その他", "hidden"=>true},
     ]
-
-    def self.raw_schema
-      if Arima::App.settings.respond_to?(:output_schema)
-        p Arima::App.settings.output_schema
-        return Arima::App.settings.output_schema
-      end
-
-      DEFAULT_SCHEMA
-    end
-    def self.schema
-      raw_schema.map do |column_info|
-        Arima::Column.new(column_info)
-      end
-    end
   end
 end
--- spec/arima/output_sepc.rb
+++ spec/arima/output_sepc.rb
@@ -1,71 +1,13 @@
 require 'spec_helper'
 
 describe Arima::Output do
-  describe ".raw_schema" do
-    context "with config" do
-      let :output_schema do
-        [{
-          "name"          => "column.name",
-          "jp"            => "column.jp",
-          "type"          => "column.type",
-          "placeholder"   => "column.placeholder",
-          "hidden"        => true,
-          "default_value" => "column.default_value",
-        }]
-      end
-
-      before do
-        schema = output_schema
-        allow(Arima::App).to receive(:output_schema) { schema }
-      end
-
-      it 'returns array' do
-        expect(Arima::Output.raw_schema).to eq output_schema
-      end
-    end
-
-    context "without config" do
-      it 'returns DEFAULT_SCHEMA' do
-        expect(Arima::Output.raw_schema).to eq Arima::Output::DEFAULT_SCHEMA
-      end
-    end
+  it "extend Arima::Schema" do
+    expect(Arima::Output).to be_kind_of(Arima::Schema)
   end
-
-  describe ".schema" do
-    context "with config" do
-      let :output_schema do
-        [{
-          "name"          => "column.name",
-          "jp"            => "column.jp",
-          "type"          => "column.type",
-          "placeholder"   => "column.placeholder",
-          "hidden"        => true,
-          "default_value" => "column.default_value",
-        }]
-      end
-
-      before do
-        schema = output_schema
-        allow(Arima::App).to receive(:output_schema) { schema }
-      end
-
-      it "returns one" do
-        expect(Arima::Output.schema.size).to eq 1
-      end
-
-      it 'returns array with Arima::Column object' do
-        expect(Arima::Output.schema[0]).to be_kind_of(Arima::Column)
-      end
-    end
-
-    context "without config" do
-      it "returns Arima::Output::DEFAULT_SCHEMA.size" do
-        expect(Arima::Output.schema.size).to eq Arima::Output::DEFAULT_SCHEMA.size
-      end
-
-      it 'returns array with Arima::Column object' do
-        expect(Arima::Output.schema.all? {|v| v.kind_of?(Arima::Column) }).to eq true
-      end
-    end
+  it "set default_schema" do
+    expect(Arima::Output.default_schema.size).to eq 4
+  end
+  it "set schema_name" do
+    expect(Arima::Output.schema_name).to eq :output_schema
   end
 end

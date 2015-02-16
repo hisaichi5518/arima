4284b60aa469ddcac092fd64a353a857fa6674eb
hisaichi5518 <hisaichi5518@gmail.com>
2015-02-16 11:29:59 +0900

    Added Arima::Brand.title spec

--- spec/arima/brand_spec.rb
+++ spec/arima/brand_spec.rb
@@ -23,4 +23,27 @@ describe Arima::Brand do
     end
 
   end
+
+  describe ".title" do
+    context "with config" do
+      before do
+        allow(Arima::App).to receive(:brand) { {"name" => "TestName"} }
+      end
+
+      it 'returns settings.brand.name' do
+        expect(Arima::Brand.title).to eq "TestName - 行動ログ閲覧"
+      end
+    end
+
+    context "without config" do
+      before do
+        allow(Arima::App).to receive(:brand) { {} }
+      end
+
+      it 'returns default name' do
+        expect(Arima::Brand.title).to eq "有馬 - 行動ログ閲覧"
+      end
+    end
+
+  end
 end

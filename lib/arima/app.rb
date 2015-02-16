41a6407f33400f9d296ca09dc066fc5fd886147f
hisaichi5518 <hisaichi5518@gmail.com>
2015-02-16 10:19:42 +0900

    use Sinatra::ConfigFile

--- lib/arima/app.rb
+++ lib/arima/app.rb
@@ -1,7 +1,16 @@
 require 'sinatra/json'
+require "sinatra/config_file"
+require 'pathname'
 
 module Arima
   class App < Sinatra::Application
+    set :root, Pathname.new(__FILE__).parent.parent.parent
+    set :views, "./views"
+    set :public_folder, "./public"
+
+    register Sinatra::ConfigFile
+    config_file 'config/config.yml'
+
     get '/' do
       json logs: []
     end
